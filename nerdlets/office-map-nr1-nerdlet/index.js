3f53a38641795eeca731bba6c68ae67702825584
Marcos Wright-Kuhns <mwrightkuhns@newrelic.com>
2019-12-06 23:28:26 -0700

    Add a pair of fake maps and some initial styling

--- nerdlets/office-map-nr1-nerdlet/index.js
+++ nerdlets/office-map-nr1-nerdlet/index.js
@@ -1,11 +1,38 @@
 import React from 'react'
 
-import { Tabs } from 'nr1'
+import { Spacing, TextField, Tabs, TabsItem } from 'nr1'
 
+import bdu1 from './maps/bdu-1.png'
+import bdu2 from './maps/bdu-2.png'
+
+const maps = [
+  {
+    id: 'bdu-1',
+    name: 'Boulder 1',
+    image: bdu1
+  }, {
+    id: 'bdu-2',
+    name: 'Boulder 2',
+    image: bdu2
+  }
+]
 // https://docs.newrelic.com/docs/new-relic-programmable-platform-introduction
 
 export default class OfficeMapNr1NerdletNerdlet extends React.Component {
   render () {
-    return <Tabs>Hello, office-map-nr1-nerdlet Nerdlet!</Tabs>
+    return <React.Fragment>
+      <TextField
+        type="search"
+        className="globalSearch"
+        placeholder='Search for a room by name, type, city, etc'
+      />
+      <Tabs>
+        {maps.map(({ id, name, image }) => (
+          <TabsItem key={id} value={id} label={name}>
+            <img src={image} alt={name} />
+          </TabsItem>
+        ))}
+      </Tabs>
+    </React.Fragment>
   }
 }
